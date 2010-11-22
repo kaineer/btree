f2f58c5401d477602011599c0e98b45e9a3886e5
Tangerine Cat <kaineer@gmail.com>
2010-11-22 15:27:36 +0500

    Added more tests [red]

--- test/btree/test_node.rb
+++ test/btree/test_node.rb
@@ -6,13 +6,22 @@ describe "BTree node" do
     assert_nothing_raised {|| BTree::Node.new }
   end
 
-  it "should be empty when created" do
-    node = BTree::Node.new
-    node.should.be.empty?
-  end
+  describe "Simple value adding" do
+    before do
+      @node = BTree::Node.new
+    end
 
-  it "should be capable of adding values" do
-    node = BTree::Node.new
-    assert_nothing_raised {|| node.add_value( 10 )}
+    it "should be empty when created" do
+      @node.should.be.empty?
+    end
+
+    it "should be capable of adding values" do
+      assert_nothing_raised {|| @node.add_value( 10 )}
+    end
+
+    it "should not be empty after value is added" do
+      @node.add_value( 10 )
+      @node.should.not.be.empty?
+    end
   end
 end
