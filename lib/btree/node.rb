84e4af945b212e0139814ad1fa90461106acba07
Tangerine Cat <kaineer@gmail.com>
2010-11-23 12:09:44 +0500

    Started tree support

--- lib/btree/node.rb
+++ lib/btree/node.rb
@@ -2,15 +2,45 @@
 #
 module BTree
   class Node
-    def initialize
+    def initialize( tree = nil )
+      @tree = tree
+      @values = []
     end
 
     def empty?
-      true
+      @values.empty?
     end
     
-    def add_value( *args )
+    def add_value( value )
+      pos = 0
+
+      while value > @values[ pos ]
+        pos += 1
+        break if pos >= @values.size
+      end unless @values.empty?
+
+      if pos >= @values.size
+        @values << value
+      else
+        @values.insert( pos, value )
+      end
+
+      self.elements_per_node
+      leaf?
     end
 
+    attr_reader :values
+    attr_reader :tree
+
+  protected
+    #
+    def elements_per_node
+      @tree.elements_per_node if @tree
+    end
+
+    #
+    def leaf?
+      true
+    end
   end
 end
--- test/btree/test_node.rb
+++ test/btree/test_node.rb
@@ -2,11 +2,19 @@ require File.join( File.dirname( __FILE__ ), "../test_helper" )
 require 'btree/node'
 
 describe "BTree node" do
-  it "should be created with constructor" do
-    assert_nothing_raised {|| BTree::Node.new }
+  describe "Node creation" do
+    it "should be created with constructor" do
+      assert_nothing_raised {|| BTree::Node.new }
+    end
+
+    it "should be created with first parameter @tree" do
+      @node = BTree::Node.new( "tree-tag" )
+      
+      @node.tree.should.be == "tree-tag"
+    end
   end
 
-  describe "Simple value adding" do
+  describe "Simple value adding without tree" do
     before do
       @node = BTree::Node.new
     end
@@ -23,5 +31,46 @@ describe "BTree node" do
       @node.add_value( 10 )
       @node.should.not.be.empty?
     end
+
+    it "should give out values added" do
+      @node.add_value( 10 )
+      @node.add_value( 20 )
+      @node.values.should.be == [ 10, 20 ]
+    end
+
+    it "should give out values sorted" do
+      @node.add_value( 20 )
+      @node.add_value( 30 )
+      @node.add_value( 10 )
+      @node.values.should.be == [ 10, 20, 30 ]
+    end
   end
+
+
+  describe "Addition inside of tree" do
+    before do
+      @tree = mock( "tree" )
+      @node = BTree::Node.new( @tree )
+    end
+
+    it "should access tree's elements per node value when adding value" do
+      @tree.expects( :elements_per_node ).returns( 11 )
+      @node.stubs( :leaf? )
+      @node.add_value( 10 )
+    end
+
+    it "should access leaf? method when adding value" do
+      @tree.stubs( :elements_per_node ).returns( 11 )
+      @node.expects( :leaf? ).returns( true )
+      @node.add_value( 10 )
+    end
+
+    it "should access subnode when leaf? returns false" do
+      @tree.stubs( :elements_per_node ).returns( 11 )
+      @node.stubs( :leaf? ).returns( false )
+      # TODO
+    end
+  end
+
+
 end
